5d6a00b8473e29c96b94fc51b34f24f70f604efb
Dirk van Meerveld <d.v.meerveld@gmail.com>
2023-02-23 20:55:36 +0900

    temporary routing and pages implemented

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from projects import views as projects
 
 urlpatterns = [
-    path('', views.pages_home, name='pages_home'),
+    path('', projects.project_index, name='project_index'),
+    path('home/', views.pages_home, name='pages_home'),
 ]

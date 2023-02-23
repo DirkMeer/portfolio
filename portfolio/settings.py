2ad63c4c9445dcfb852a4dec402d7247291fe385
Dirk van Meerveld <d.v.meerveld@gmail.com>
2023-02-23 20:38:33 +0900

    Fix wsgi file path

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -1,7 +1,5 @@
-import dj_database_url
 from decouple import config
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

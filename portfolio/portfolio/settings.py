58a72c8350ff7af621c1ec6fc59d642472353095
Dirk van Meerveld <d.v.meerveld@gmail.com>
2023-02-23 20:09:45 +0900

    fix settings bug

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -1,4 +1,3 @@
-import django_heroku
 import dj_database_url
 from decouple import config
 from pathlib import Path
@@ -124,6 +123,3 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-django_heroku.settings(locals)
